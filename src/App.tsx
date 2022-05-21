c36743f31e09b4160ca5326c1cbd93c604ef65ec
Eniola Ajayi <ajayieniola16@gmail.com>
2022-05-21 10:37:57 +0100

    add default routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { MantineProvider } from "@mantine/core";
 import HomePage from "./routes/HomePage";
 import { ModalsProvider } from "@mantine/modals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./routes/Dashboard";
+import NotFoundPage from "./routes/NotFoundPage";
 
 function App() {
   return (
@@ -12,7 +15,13 @@ function App() {
       withNormalizeCSS
     >
       <ModalsProvider>
-        <HomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="dashboard" element={<Dashboard />}></Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </BrowserRouter>
       </ModalsProvider>
     </MantineProvider>
   );
